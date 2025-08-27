ede0620b9c2fae2f80131fccaa57cc023ba0c7a1
raghuvansh0 <pbarasiadb@gmail.com>
2025-08-27 21:28:11 +0530

    Update ott-1.js

--- ott-1.js
+++ ott-1.js
@@ -470,7 +470,7 @@ function buildTheaterScreen(mode = 'phone') {
     // flat screen can be moved in front of camera
     screen.position.set(...config.screenPosition);
   } else {
-    screen.rotation.y = Math.PI;
+    screen.rotation.x = Math.PI;
     // curved screen must stay centered on origin so camera is inside it
     //screen.position.set(0, 0, 0);
   }
